a61afbffc9514d628fd8fd2923579cc73693bdf1
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2019-09-12 17:51:55 -0500

    mysql uri and procfile added

--- server/middleware/database.js
+++ server/middleware/database.js
@@ -6,6 +6,6 @@ const options = {
     password: 'admin',
     database: 'db_attentionapp'
 };
-const pool = mysql.createPool(options);
+const pool = mysql.createPool("mysql://udm1bwpybxtsbyvw:JgNKhwqSVu213JMDcf9d@bbnv9jwejwgt0rmpt5nb-mysql.services.clever-cloud.com:3306/bbnv9jwejwgt0rmpt5nb");
 
 module.exports = pool
