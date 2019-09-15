27f66bc35f0813f3a608176a061b69887bf1da1b
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2019-09-14 22:50:33 -0500

    Added reservations endpoints

---
+++ server/models/reservation.js
@@ -0,0 +1,55 @@
+const { escape } = require('mysql');
+const pool = require('../middleware/database');
+
+module.exports = {
+    findById: (id,callback) => {
+        const sql = `SELECT * FROM reservations WHERE idreservation = ${escape(id)} AND active=1`;
+        pool.query(sql,(err,rows) => {
+            if (err){
+                callback(err,null);
+            }
+            callback(err,rows[0]);
+        });
+    },
+    findAll: callback => {
+        const sql = 'SELECT * FROM reservations WHERE active=1';
+        pool.query(sql, (err,rows) => {
+            if (err){
+                callback(err,null);
+            }
+            callback(err,rows);
+        });
+    },
+    save: (reservationData, callback) => {
+        const sql = 'INSERT INTO reservations SET ?';
+        pool.query(sql,reservationData,(err,results) => {
+            if (err){
+                callback(err,null);
+            }
+            console.log(`Affected rows: ${results.affectedRows}`);
+            callback(err,results.insertId);
+        });
+    },
+    update: (id,reservationData,callback) => {
+        const sql = 'UPDATE reservations SET ? WHERE idreservation = ?';
+        pool.query(sql,[reservationData,id],(err,results) => {
+            if (err){
+                callback(err,null);
+            }
+            const message = `Changed ${results.changedRows} row(s)`;
+            console.log(message);
+            callback(err,message);
+        });
+    },
+    delete: (id,payload,callback) => {
+        const sql = 'UPDATE reservations SET ? WHERE idreservation = ?';
+        pool.query(sql,[payload,id],(err,results) => {
+            if (err){
+                callback(err,null);
+            }
+            const message = `Deleted ${results.affectedRows} rows`;
+            console.log(message);
+            callback(err,message);
+        });
+    }
+}
---
+++ server/routes/v1/reservation-router.js
@@ -0,0 +1,234 @@
+const { Router } = require('express');
+const reservationRepo = require('../../models/reservation');
+const auth = require('../../middleware/auth');
+const reservationRouter = Router();
+
+/**
+ * @swagger
+ * /api/v1/reservations:
+ *   get:
+ *     tags:
+ *       - reservations
+ *     description: Returns all reservations
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer Token
+ *         type: string
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of reservations
+ *       401:
+ *         description: Unauthorized
+ */
+reservationRouter.get('/',auth.verifyToken,(req,res) => {
+    reservationRepo.findAll((err,rows) => {
+        if(err) throw err;
+        res.status(200).send({ success: true, numRows: rows.length, rows});
+    });
+});
+
+/**
+ * @swagger
+ * /api/v1/reservations/{id}:
+ *   get:
+ *     tags:
+ *       - reservations
+ *     description: Returns reservation by id
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer Token
+ *         type: string
+ *       - name: id
+ *         description: Reservation Id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: A reservation
+ *       401:
+ *         description: Unauthorized
+ */
+reservationRouter.get('/:id',auth.verifyToken,(req,res) => {
+    const id = parseInt(req.params.id);
+    reservationRepo.findById(id,(err,customer) => {
+        let numRows = 0;
+        if(err) throw err;
+        if(customer) numRows++;
+        res.status(200).send({ success: true, numRows, customer});
+    });
+});
+
+
+/**
+ * @swagger
+ * /api/v1/reservations/:
+ *   post:
+ *     tags:
+ *       - reservations
+ *     description: Add a reservation
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer Token
+ *         type: string
+ *       - name: idcustomer
+ *         type: integer
+ *         in: formData
+ *         required: true
+ *       - name: idnurse
+ *         type: integer
+ *         in: formData
+ *         required: true
+ *       - name: idcard
+ *         type: integer
+ *         in: formData
+ *         required: true
+ *       - name: location
+ *         type: string
+ *         in: formData
+ *         required: false
+ *       - name: start_date
+ *         type: string
+ *         in: formData
+ *         required: true
+ *       - name: end_date
+ *         type: string
+ *         in: formData
+ *         required: true
+ *       - name: amount
+ *         type: number
+ *         in: formData
+ *         required: false
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Insert Id
+ *       401:
+ *         description: Unauthorized
+ */
+reservationRouter.post('/',auth.verifyToken, (req,res) => {
+    let reservationData = req.body;
+    const commonProps = {
+        created_by: req.userData.email,
+        create_date: new Date(),
+        active: true
+    };
+    reservationData = Object.assign(reservationData,commonProps);
+    reservationRepo.save(reservationData,(err,insertId) => {
+        if(err) throw err;
+        res.status(201).send({success: true ,insertId});
+    });
+});
+
+/**
+ * @swagger
+ * /api/v1/reservations/{id}:
+ *   put:
+ *     tags:
+ *       - reservations
+ *     description: Update a reservation
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer Token
+ *         type: string
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *       - name: idcustomer
+ *         type: integer
+ *         in: formData
+ *         required: true
+ *       - name: idnurse
+ *         type: integer
+ *         in: formData
+ *         required: true
+ *       - name: idcard
+ *         type: integer
+ *         in: formData
+ *         required: true
+ *       - name: location
+ *         type: string
+ *         in: formData
+ *         required: false
+ *       - name: start_date
+ *         type: string
+ *         in: formData
+ *         required: true
+ *       - name: end_date
+ *         type: string
+ *         in: formData
+ *         required: true
+ *       - name: amount
+ *         type: number
+ *         in: formData
+ *         required: false
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: A message if completes successfully
+ *       401:
+ *         description: Unauthorized
+ */
+reservationRouter.put('/:id', auth.verifyToken, (req,res) => {
+    let reservationData = req.body;
+    const id = parseInt(req.params.id);
+    const commonProps = {
+        modified_by: req.userData.email,
+        modify_date: new Date()
+    };
+    reservationData = Object.assign(reservationData,commonProps);
+    reservationRepo.update(id,reservationData,(err,message) => {
+        if(err) throw err;
+        res.status(200).send({success: true, message});
+    })
+});
+
+/**
+ * @swagger
+ * /api/v1/reservations/{id}:
+ *   delete:
+ *     tags:
+ *       - reservations
+ *     description: Delete a reservation
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer Token
+ *         type: string
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: A message if completes successfully
+ *       401:
+ *         description: Unauthorized
+ */
+reservationRouter.delete('/:id', auth.verifyToken, (req,res) => {
+    const id = parseInt(req.params.id);
+    const payload = {
+        active: false,
+        deleted_by: req.userData.email,
+        delete_date: new Date()
+    };
+    reservationRepo.delete(id,payload,(err,message) => {
+        if(err) throw err;
+        res.status(200).send({success: true, message});
+    });
+});
+
+module.exports = reservationRouter;
