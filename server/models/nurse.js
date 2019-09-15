978d1676bbfc9e96a5589f15a301a9a93399d670
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2019-09-14 22:24:14 -0500

    nurses endpoints fixed

--- server/models/nurse.js
+++ server/models/nurse.js
@@ -5,18 +5,27 @@ module.exports = {
     findById: (id,callback) => {
         const sql = `SELECT * FROM nurses WHERE idnurse = ${escape(id)} AND active=1`;
         pool.query(sql,(err,rows) => {
+            if(err){
+                callback(err,null);
+            }
             callback(err,rows[0]);
         });
     },
     findAll: callback => {
         const sql = 'SELECT * FROM nurses WHERE active=1';
         pool.query(sql,(err,rows) => {
+            if(err){
+                callback(err,null);
+            }
             callback(err,rows);
         });
     },
     save: (nurseData,callback) => {
         const sql = 'INSERT INTO nurses SET ?';
         pool.query(sql,nurseData,(err,results) => {
+            if(err){
+                callback(err,null);
+            }
             console.log(`Affected rows: ${results.affectedRows}`);
             callback(err,results.insertId);
         });
@@ -24,14 +33,20 @@ module.exports = {
     update: (id,nurseData,callback) => {
         const sql = `UPDATE nurses SET ? WHERE idnurse = ?`;
         pool.query(sql,[nurseData,id],(err,results) => {
+            if(err){
+                callback(err,null);
+            }
             const message = `Changed ${results.changedRows} row(s)`;
             console.log(message);
             callback(err,message);
         });
     },
-    delete: (id,callback) => {
-        const sql = `DELETE FROM nurses WHERE idnurse=${escape(id)}`;
-        pool.query(sql,(err,results) => {
+    delete: (id,payload,callback) => {
+        const sql = `UPDATE nurses SET ? WHERE idnurse = ?`;
+        pool.query(sql,[payload,id],(err,results) => {
+            if(err){
+                callback(err,null);
+            }
             const message = `Deleted ${results.affectedRows} rows`;
             console.log(message);
             callback(err,message);
--- server/routes/v1/nurse-router.js
+++ server/routes/v1/nurse-router.js
@@ -4,6 +4,26 @@ const auth = require('../../middleware/auth');
 
 const nurseRouter = Router();
 
+/**
+ * @swagger
+ * /api/v1/nurses:
+ *   get:
+ *     tags:
+ *       - nurses
+ *     description: Returns all nurses
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer Token
+ *         type: string
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of nurses
+ *       401:
+ *         description: Unauthorized
+ */
 nurseRouter.get('/',auth.verifyToken, (req,res) => {
     nurseRepo.findAll((err,rows) => {
         if(err) throw err;
@@ -11,14 +31,102 @@ nurseRouter.get('/',auth.verifyToken, (req,res) => {
     });
 });
 
+/**
+ * @swagger
+ * /api/v1/nurses/{id}:
+ *   get:
+ *     tags:
+ *       - nurses
+ *     description: Returns nurse by id
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer Token
+ *         type: string
+ *       - name: id
+ *         description: Nurse Id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Nurse Data
+ *       401:
+ *         description: Unauthorized
+ */
 nurseRouter.get('/:id',auth.verifyToken,(req,res) => {
     const id = parseInt(req.params.id);
     nurseRepo.findById(id,(err,nurse) => {
+        let numRows = 0;
         if(err) throw err;
-        res.status(200).send({ success: true, numRows: 1, nurse});
+        if(nurse) numRows++;
+        res.status(200).send({ success: true, numRows, nurse});
     });
 });
 
+
+/**
+ * @swagger
+ * /api/v1/nurses/:
+ *   post:
+ *     tags:
+ *       - nurses
+ *     description: Add a nurse
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer Token
+ *         type: string
+ *       - name: first_name
+ *         type: string
+ *         in: formData
+ *         required: true
+ *       - name: last_name
+ *         type: string
+ *         in: formData
+ *         required: true
+ *       - name: short_name
+ *         type: string
+ *         in: formData
+ *         required: true
+ *       - name: code_identification
+ *         type: string
+ *         in: formData
+ *         required: true
+ *       - name: gender
+ *         type: string
+ *         in: formData
+ *         required: true
+ *       - name: qualification
+ *         type: number
+ *         in: formData
+ *         required: false
+ *       - name: thumbnail_image
+ *         type: string
+ *         in: formData
+ *         required: false
+ *       - name: comment
+ *         type: string
+ *         in: formData
+ *         required: false
+ *       - name: description
+ *         type: string
+ *         in: formData
+ *         required: false
+ *       - name: idnursetype
+ *         type: integer
+ *         in: formData
+ *         required: true
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Insert Id
+ *       401:
+ *         description: Unauthorized
+ */
 nurseRouter.post('/',auth.verifyToken,(req,res) => {
     let nurseData = req.body;
     const commonProps = {
@@ -33,13 +141,76 @@ nurseRouter.post('/',auth.verifyToken,(req,res) => {
     });
 });
 
+/**
+ * @swagger
+ * /api/v1/nurses/{id}:
+ *   put:
+ *     tags:
+ *       - nurses
+ *     description: Update a nurse
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer Token
+ *         type: string
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *       - name: first_name
+ *         type: string
+ *         in: formData
+ *         required: true
+ *       - name: last_name
+ *         type: string
+ *         in: formData
+ *         required: true
+ *       - name: short_name
+ *         type: string
+ *         in: formData
+ *         required: true
+ *       - name: code_identification
+ *         type: string
+ *         in: formData
+ *         required: true
+ *       - name: gender
+ *         type: string
+ *         in: formData
+ *         required: true
+ *       - name: qualification
+ *         type: number
+ *         in: formData
+ *         required: false
+ *       - name: thumbnail_image
+ *         type: string
+ *         in: formData
+ *         required: false
+ *       - name: comment
+ *         type: string
+ *         in: formData
+ *         required: false
+ *       - name: description
+ *         type: string
+ *         in: formData
+ *         required: false
+ *       - name: idnursetype
+ *         type: integer
+ *         in: formData
+ *         required: true
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: A message if complete successfully
+ *       401:
+ *         description: Unauthorized
+ */
 nurseRouter.put('/:id', auth.verifyToken, (req,res) => {
     let nurseData = req.body;
     const id = parseInt(req.params.id);
     const commonProps = {
         modified_by: req.userData.email,
-        modify_date: new Date(),
-        modify_reason: 'UPDATE'
+        modify_date: new Date()
     };
     nurseData = Object.assign(nurseData,commonProps);
     nurseRepo.update(id,nurseData,(err,message) => {
@@ -48,9 +219,38 @@ nurseRouter.put('/:id', auth.verifyToken, (req,res) => {
     });
 });
 
+/**
+ * @swagger
+ * /api/v1/nurses/{id}:
+ *   delete:
+ *     tags:
+ *       - nurses
+ *     description: Delete a nurse
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer Token
+ *         type: string
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: A message if complete successfully
+ *       401:
+ *         description: Unauthorized
+ */
 nurseRouter.delete('/:id',auth.verifyToken, (req,res) => {
     const id = parseInt(req.params.id);
-    nurseRepo.delete(id,(err,message) => {
+    const payload = {
+        active: false,
+        deleted_by: req.userData.email,
+        delete_date: new Date()
+    }
+    nurseRepo.delete(id,payload,(err,message) => {
         if(err) throw err;
         res.status(200).send({success: true, message});
     });
